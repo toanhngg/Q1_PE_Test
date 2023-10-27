15f13919a42f2791204680598c0304a4dad4f69a
Toanh <anhng130@gmail.com>
2023-10-27 16:45:30 +0700

    add Q1

--- Q1_PE_Test/App.xaml.cs
+++ Q1_PE_Test/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Q1_PE_Test.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,15 @@ namespace Q1_PE_Test
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; set; }
+        public IConfiguration Configuration { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceColection = new ServiceCollection();
+            serviceColection.AddTransient<MainWindow>();
+            serviceColection.AddScoped<PRN221_Spr22Context>();
+            ServiceProvider = serviceColection.BuildServiceProvider();
+            ServiceProvider.GetRequiredService<MainWindow>().Show();
+        }
     }
 }
--- Q1_PE_Test/MainWindow.xaml.cs
+++ Q1_PE_Test/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Q1_PE_Test.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,143 @@ namespace Q1_PE_Test
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly PRN221_Spr22Context _context;
+
+        public MainWindow(PRN221_Spr22Context context)
         {
             InitializeComponent();
+            _context = context;
+            LoadData();
+        }
+        public void LoadData()
+        {
+            listEmployee.ItemsSource = _context.Employees.ToList();
+        }
+        public Employee GetEmployeeObject()
+        {
+            try
+            {
+                return new Employee
+                {
+                    Id = string.IsNullOrEmpty(employeeId.Text) ? 0 : int.Parse(employeeId.Text),
+                    Name = employeeName.Text,
+                    Gender = male.IsChecked == true ? "Male" : "Female",
+                    Dob = dob.SelectedDate,
+                    Phone = phone.Text,
+                    Idnumber = idnumber.Text,
+
+                };
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot get Employee", "Get Employee");
+            }
+            return null;
+        }
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            employeeId.Text = string.Empty;
+            employeeName.Text = string.Empty;
+            male.IsChecked = true;
+            phone.Text = string.Empty;
+            idnumber.Text = string.Empty;
+            dob.SelectedDate = null;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var employee = GetEmployeeObject();
+                if (employee != null)
+                {
+                    employee.Id = 0;
+                    _context.Employees.Add(employee);
+                    _context.SaveChanges();
+                    LoadData();
+                    MessageBox.Show("Add employee success", "Add employee");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Insert employee fail", "Add Employee");
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var employee = GetEmployeeObject();
+                if (employee != null)
+                {
+                    var oldEmployee = _context.Employees.FirstOrDefault(x => x.Id == employee.Id);
+                    if (oldEmployee != null)
+                    {
+                        oldEmployee.Dob = employee.Dob;
+                        oldEmployee.Phone = employee.Phone;
+                        oldEmployee.Name = employee.Name;
+                        oldEmployee.Idnumber = employee.Idnumber;
+                        oldEmployee.Gender = employee.Gender;
+
+                        _context.Employees.Update(oldEmployee);
+                        _context.SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Update employee success", "Update employee");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update employee fail", "Update Employee");
+
+            }
+        }
+        private void listView_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+            if (item != null)
+            {
+                var gender = ((Employee)item).Gender;
+                if (!string.IsNullOrEmpty(gender))
+                {
+                    if (gender.ToLower() == "female")
+                    {
+                        female.IsChecked = true;
+                    }
+                    else
+                    {
+                        male.IsChecked = true;
+                    }
+                }
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var employee = GetEmployeeObject();
+                if (employee != null)
+                {
+                    var oldEmployee = _context.Employees.FirstOrDefault(x => x.Id == employee.Id);
+                    if (oldEmployee != null)
+                    {
+                       _context.Employees.Remove(oldEmployee);
+                        _context.SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Delete employee success", "Delete employee");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Delete employee fail", "Delete Employee");
+
+            }
         }
     }
 }
